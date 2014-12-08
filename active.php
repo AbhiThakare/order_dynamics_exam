281703955fb6e05055cf9085f76ad0b12c410bf0
AbhiThakare <thakareabhinav@gmial.com>
2014-12-08 23:06:08 +0530

    initial file setup

---
+++ active.php
@@ -0,0 +1,13 @@
+// Active.PHP
+<?php
+
+set_time_limit(20);
+
+while ($i=0)
+{
+        echo "i=$i ";
+        sleep(100);
+        $i--;
+}
+
+?>
---
+++ temp.php
@@ -0,0 +1,5 @@
+//Temp.PHP
+
+<?php
+	echo $r;
+?>
